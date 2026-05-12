4b4529ae218d9edc944f74f425dce622e6147b60
FURYMD <sithija800@gmail.com>
2026-05-12 16:16:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "5McUyI4D#NwF3TfNY78K0Yi-8rGD7EVES0Pzw7rSgKH7pNS40fd8",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/FURYMD/NETHUM-MD/blob/main/images/ChatGPT%20Image%20May%2012,%202026,%2003_46_11%20PM.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 NETHUM-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+BOT_OWNER: '94721640007',  // Replace with the owner's phone number
+AUTO_STATUS_SEEN: 'true',
+AUTO_STATUS_REACT: 'true',
 
 
 
