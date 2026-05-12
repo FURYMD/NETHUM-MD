fac3822fb5ad29e26f9f7fc77c9a347b33350cd3
FURYMD <sithija800@gmail.com>
2026-05-12 17:49:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ async function connectToWA() {
 
       const up = `NETHUM-MD connected ✅\n\nPREFIX: ${prefix}`;
       await test.sendMessage(ownerNumber[0] + "@s.whatsapp.net", {
-        image: { url: `raw.githubusercontent.com/FURYMD/NETHUM-MD/main/images/ChatGPT%20Image%20May%2012,%202026,%2003_46_11%20PM.png` },
+        image: { url: `https://raw.githubusercontent.com/FURYMD/NETHUM-MD/main/images/ChatGPT%20Image%20May%2012,%202026,%2003_46_11%20PM.png` },
         caption: up
       });
 
